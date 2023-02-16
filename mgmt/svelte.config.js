5e4ab43e20bb24406b1cdc86e0e18c9e2a345685
Leon Kuhne <leonkuhne@gmail.com>
2023-02-16 14:51:31 -0500

    autosave

---
+++ mgmt/svelte.config.js
@@ -0,0 +1,18 @@
+import path from 'path'
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess({
+    
+}),
+	kit: {
+    alias: {
+      $components: path.resolve('/src/components'),
+    },
+		adapter: adapter()
+	}
+};
+
+export default config;
