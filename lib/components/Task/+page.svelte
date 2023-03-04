8a14ed80e9416eb2fc00de56f64c2c27c823bfe9
Leon Kuhne <leonkuhne@gmail.com>
2023-03-04 15:44:21 -0500

    added envelope

--- lib/components/Task/+page.svelte
+++ lib/components/Task/+page.svelte
@@ -1,23 +1,27 @@
 <script>
   import Duration from '$components/Duration/+page.svelte'
   export let task = null
+  export let open = false
 </script>
 
 <template lang="pug">
 +if('task')
   .task
+    .expand('on:click={() => open = !open}') {open ? 'v' : '>'}
     span {task.title}
     Duration(value='{task.duration}')
-    +each('task.children as subtask')
-      .subtask
-        svelte:self(task='{subtask}')
+    +if('open')
+      +each('task.children as subtask')
+        .subtask
+          svelte:self(task='{subtask}')
 </template>
 
 <style lang="stylus">
 .task
   width 100%
-.task::before
-  content '>'
+.expand
+  display inline-block
+  cursor pointer
   margin-right .5rem
 .subtask
   margin-left .5rem
