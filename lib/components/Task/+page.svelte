71187a54c3912aaa8f5788f7fa2fe47231d53e4f
Leon Kuhne <leonkuhne@gmail.com>
2023-03-04 18:08:29 -0500

    updated task

--- lib/components/Task/+page.svelte
+++ lib/components/Task/+page.svelte
@@ -1,25 +1,42 @@
 <script>
   import Duration from '$components/Duration/+page.svelte'
+  import Model from '$models/Task/script.js'
   export let task = null
-  export let open = false
+  let open = false
+
+  let updates = 0
+  const addTask = () => {
+    // TODO instead get the task manager from the global model and use it to add a new task
+    task.children.push({
+      title: 'New task',
+      duration: 0,
+      children: []
+    })
+    open = true // expand new subtask
+    updates++ // render update
+  }
 </script>
 
 <template lang="pug">
 +if('task')
   .task
-    .expand('on:click={() => open = !open}') {open ? 'v' : '>'}
+    +if('task.children.length')
+      .expand('on:click={() => open = !open}') {open ? 'v' : '>'}
+      +else()
+        .add('on:click={addTask}') +
     span {task.title}
     Duration(value='{task.duration}')
     +if('open')
-      +each('task.children as subtask')
-        .subtask
-          svelte:self(task='{subtask}')
+      +key('updates')
+        +each('task.children as subtask')
+          .subtask
+            svelte:self(task='{subtask}')
 </template>
 
 <style lang="stylus">
 .task
   width 100%
-.expand
+.add, .expand
   display inline-block
   cursor pointer
   margin-right .5rem
